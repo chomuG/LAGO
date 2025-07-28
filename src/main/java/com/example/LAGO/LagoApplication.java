c849f4c244f04204d4553bf6fcbc4a09180a01cd
jhpps1 <jhpps@naver.com>
2025-07-28 15:19:45 +0900

    chore: 백엔드 초기 Spring Boot 프로젝트 세팅
    - build.gradle, src/ 등 전체 구조 업로드
    - lombok, redis, JPA 등 필수 의존성 포함
    - 기본 README 및 설정파일 추가

---
+++ src/main/java/com/example/LAGO/LagoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LAGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagoApplication.class, args);
+	}
+
+}
